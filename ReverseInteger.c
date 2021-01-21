1cc6221728b5b2a2bb5e2f7d3c480c95763a1dff
ienlie0513 <39272490+ienlie0513@users.noreply.github.com>
2021-01-21 21:00:25 +0800

    Update ReverseInteger.c

--- ReverseInteger.c
+++ ReverseInteger.c
@@ -1,24 +1,12 @@
 int reverse(int x){
-    if(x==0) return 0;
-    
     long long int ans = 0;
-    long long int pos_x = x;
-    bool ifne = false;
-    
-    if(pos_x < 0){
-        pos_x = -pos_x;
-        ifne = true;
+
+    while(x != 0){
+        ans = ans*10 + x%10;
+        if(ans >= INT_MAX || ans <= INT_MIN) 
+            return 0;
+        x /= 10;
     }
-    
-    while(pos_x > 0){
-        ans *= 10;
-        int tmp = pos_x % 10;
-        ans += tmp;
-        if(ans > 2147483648) return 0;
-        pos_x /= 10;
-    }
-    
-    if(ifne) ans = -ans;
     
     return ans;
 }
