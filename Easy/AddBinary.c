a93367775c8d95486eb790e0e07d5a5723c33f08
ienlie0513 <39272490+ienlie0513@users.noreply.github.com>
2021-11-15 16:30:01 +0800

    Update AddBinary.c

--- Easy/AddBinary.c
+++ Easy/AddBinary.c
@@ -1,47 +1,33 @@
 char * addBinary(char * a, char * b){
-    int carry = 0;
-    char *ans;
+    int carry=0;
     if(strlen(a)>strlen(b)){
-        ans = (char *)calloc(strlen(a)+2, sizeof(char));
+        char *t = b;
+        b = a;
+        a = t;
+    }
+    char *ans = (char *)malloc(sizeof(char)*(strlen(b)+2));
+    ans[strlen(b)+1] = '\0';
     
-        for(int i=0; i<strlen(b); i++){
-            ans[strlen(a)-i-1] = 
-                ((a[strlen(a)-i-1] - '0') + (b[strlen(b)-i-1] - '0') + carry) % 2 + '0';
-            carry = ((a[strlen(a)-i-1] - '0') + (b[strlen(b)-i-1] - '0') + carry) / 2;
-        }
-        
-        for(int i=strlen(b); i<strlen(a); i++){
-            ans[strlen(a)-i-1] = ((a[strlen(a)-i-1] - '0') + carry) % 2 + '0';
-            carry = ((a[strlen(a)-i-1] - '0') + carry) / 2;
-        }
-        
-        if(carry == 0)
-            return ans;
-        
-        for(int i=strlen(a); i>=0; i--)
-            ans[i+1] = ans[i];
-        ans[0] = '1';
+    int i=strlen(a)-1, j=strlen(b)-1, k=strlen(b);
+    for(; i>=0; i--, j--, k--){
+        carry = (a[i]-'0') + (b[j]-'0') + carry;
+        ans[k] = carry%2 + '0';
+        carry /= 2;
+    }
+    
+    for(; j>=0; j--, k--){
+        carry = (b[j]-'0') + carry;
+        ans[k] = carry%2 + '0';
+        carry /= 2;
+    }
+    
+    if(carry){
+        ans[k] = carry + '0';
     }
     else{
-        ans = (char *)calloc(strlen(b)+2, sizeof(char));
-    
-        for(int i=0; i<strlen(a); i++){
-            ans[strlen(b)-i-1] = 
-                ((a[strlen(a)-i-1] - '0') + (b[strlen(b)-i-1] - '0') + carry) % 2 + '0';
-            carry = ((a[strlen(a)-i-1] - '0') + (b[strlen(b)-i-1] - '0') + carry) / 2;
+        for(;k<strlen(b)+1; k++){
+            ans[k] = ans[k+1];
         }
-        
-        for(int i=strlen(a); i<strlen(b); i++){
-            ans[strlen(b)-i-1] = ((b[strlen(b)-i-1] - '0') + carry) % 2 + '0';
-            carry = ((b[strlen(b)-i-1] - '0') + carry) / 2;
-        }
-        
-        if(carry == 0)
-            return ans;
-        
-        for(int i=strlen(b); i>=0; i--)
-            ans[i+1] = ans[i];
-        ans[0] = '1';
     }
         
     return ans;
