4ee839feacafbc008df303977a573d58072af525
ienlie0513 <39272490+ienlie0513@users.noreply.github.com>
2021-11-18 19:33:49 +0800

    Update Pascal'sTriangleII.c

--- Easy/Pascal'sTriangleII.c
+++ Easy/Pascal'sTriangleII.c
@@ -6,18 +6,17 @@
 int* getRow(int rowIndex, int* returnSize){
     *returnSize = rowIndex+1;
     int *ans = (int *)malloc(sizeof(int)*(rowIndex+1));
-    int temp[34], left, right = 1;
-    for(int i=0; i<34; i++)
-        temp[i] = 1;
-    for(int i=0; i<rowIndex+1; i++){
+    
+    ans[0] = 1;
+    int left, right=1;
+    for(int i=1;i<=rowIndex;i++){
         for(int j=1; j<i; j++){
             left = right;
-            right = temp[j];
-            temp[j] = left + right;
+            right = ans[j];
+            ans[j] = left + right;
         }
+        ans[i] = 1;
     }
-    for(int i=0; i<rowIndex+1; i++){
-        ans[i] = temp[i];
-    }
+    
     return ans;
 }
