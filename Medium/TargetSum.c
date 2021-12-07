3070e1f2933bb71149fd5805e9f3b8c2ea38fa2e
ienlie0513 <39272490+ienlie0513@users.noreply.github.com>
2021-12-07 16:55:11 +0800

    Update TargetSum.c, DP

--- Medium/TargetSum.c
+++ Medium/TargetSum.c
@@ -1,24 +1,27 @@
-int DFS(int* nums, int numsSize, int target, int i){
-    if(i == numsSize-1)
-        if(!(target+nums[i]) && !(target-nums[i]))
-            return 2;
-        else if(!(target+nums[i]) || !(target-nums[i]))
-            return 1;
-        else
-            return 0;
-    
-    int t1 = target-nums[i];
-    int t2 = target+nums[i];
-    i++;
-    return DFS(nums, numsSize, t1, i) + DFS(nums, numsSize, t2, i);
-}
-
 int findTargetSumWays(int* nums, int numsSize, int target){
     int sum=0;
     for(int i=0; i<numsSize; i++)
         sum += nums[i];
-    if(sum < target || (sum+target)%2 != 0)
+    if(sum < target || -sum > target || (sum+target)%2 != 0)
         return 0;
     
-    return DFS(nums, numsSize, target, 0);    
+    int new_target = (sum+target)/2;
+    int **dp = (int **)malloc(sizeof(int*)*(numsSize+1));
+    for(int i=0; i<numsSize+1; i++){
+        dp[i] = (int *)malloc(sizeof(int)*(new_target+1));
+        dp[i][0] = 1;
+    }
+    for(int i=1; i<new_target+1; i++)
+        dp[0][i] = 0;
+
+    for(int i=1; i<numsSize+1; i++){
+        for(int j=0; j<new_target+1; j++){
+            if(j-nums[i-1] >= 0)
+                dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+    
+    return dp[numsSize][new_target];    
 }
