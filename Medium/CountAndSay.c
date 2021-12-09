a5d5c838285ad52c60244ce650849d0edf404920
ienlie0513 <39272490+ienlie0513@users.noreply.github.com>
2021-12-09 16:31:49 +0800

    Update CountAndSay.c

--- Medium/CountAndSay.c
+++ Medium/CountAndSay.c
@@ -1,22 +1,28 @@
 
 
 char * countAndSay(int n){
-    if(n == 1)
-        return "1\0";
+    char *ans = calloc(18000, sizeof(char));
+    char *tmp = calloc(18000, sizeof(char));
     
-    char *s = countAndSay(n-1);
-    int cnt, idx=0;
+    int cnt, idx;
     char prev;
-    char *ans = calloc(5000, sizeof(char));
-    while(*s != '\0'){
-        prev = *s;
-        cnt = 0;
-        while(*s == prev){
-            cnt++;
-            *s++;
+    *tmp = '1';
+    *ans = '1';
+    
+    for(int i=2; i<=n; i++){
+        idx = 0;
+        while(*tmp != '\0'){
+            prev = *tmp;
+            cnt = 0;
+            while(*tmp == prev){
+                cnt++;
+                *tmp++;
+            }
+            ans[idx++] = cnt + '0';
+            ans[idx++] = prev;
         }
-        ans[idx++] = cnt + '0';
-        ans[idx++] = prev;
+        ans[idx] = '\0';
+        strcpy(tmp, ans);
     }
     
     return ans;
