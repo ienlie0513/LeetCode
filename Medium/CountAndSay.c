e124fe4fae50f2016d291b98f24bded77fb57aa9
ienlie0513 <39272490+ienlie0513@users.noreply.github.com>
2021-12-09 16:06:25 +0800

    Update CountAndSay.c, recursive

--- Medium/CountAndSay.c
+++ Medium/CountAndSay.c
@@ -1,29 +1,23 @@
+
+
 char * countAndSay(int n){
-    if(n == 1){
-        char *ans = (char *)malloc(sizeof(char)*4500);
-        for(int i=0; i<4500; i++)
-            ans[i] = '\0';
-        ans[0] = '1';
-        return ans;
-    }
-    else{
-        char *ans = countAndSay(n-1);
-        char word[strlen(ans)+1];
-        strcpy(word, ans);
-        int index = 0;
-        int ans_index = 0;
-        int i;
-        for(i=0; i<strlen(word); i++){
-            if(word[i] != word[index]){
-                ans[ans_index] = i - index + 48;
-                ans[ans_index+1] = word[index];
-                index = i;
-                ans_index += 2;
-            }
+    if(n == 1)
+        return "1\0";
+    
+    char *s = countAndSay(n-1);
+    int cnt, idx=0;
+    char prev;
+    char *ans = calloc(5000, sizeof(char));
+    while(*s != '\0'){
+        prev = *s;
+        cnt = 0;
+        while(*s == prev){
+            cnt++;
+            *s++;
         }
-        ans[ans_index] = i - index + 48;
-        ans[ans_index+1] = word[index];
-        return ans;
+        ans[idx++] = cnt + '0';
+        ans[idx++] = prev;
     }
-    return '0';
+    
+    return ans;
 }
