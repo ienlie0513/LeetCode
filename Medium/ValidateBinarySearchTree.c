307e30508a748ab14ee99b06065b9c6b24f97ce2
ienlie0513 <39272490+ienlie0513@users.noreply.github.com>
2021-12-14 10:56:28 +0800

    Update ValidateBinarySearchTree.c

--- Medium/ValidateBinarySearchTree.c
+++ Medium/ValidateBinarySearchTree.c
@@ -7,65 +7,16 @@
  * };
  */
 
-int maxVal(struct TreeNode* root){
-    if(root->left == NULL && root->right == NULL)
-        return root->val;
-    
-    if(root->left == NULL){
-        int right = maxVal(root->right);
-        return right>root->val ? right: root->val;
-    }
-    if(root->right == NULL){
-        int left = maxVal(root->left);
-        return left>root->val ? left: root->val;
-    }
-    
-    int left = maxVal(root->left);
-    int right = maxVal(root->right);
-    
-    if(left>right && left>root->val)
-        return left;
-    if(right>left && right>root->val)
-        return right;
-    
-    return root->val;
-}
-
-int minVal(struct TreeNode* root){
-    if(root->left == NULL && root->right == NULL)
-        return root->val;
-    
-    if(root->left == NULL){
-        int right = minVal(root->right);
-        return right<root->val ? right: root->val;
-    }
-    if(root->right == NULL){
-        int left = minVal(root->left);
-        return left<root->val ? left: root->val;
-    }
-    
-    int left = minVal(root->left);
-    int right = minVal(root->right);
-    
-    if(left<right && left<root->val)
-        return left;
-    if(right<left && right<root->val)
-        return right;
-    
-    return root->val;
-}
-
-
-bool isValidBST(struct TreeNode* root){
+bool valid(struct TreeNode* root, long long int max, long long int min){
     if(root == NULL)
         return true;
-
-    // max from left child < me
-    if(root->left != NULL && maxVal(root->left) >= root->val)
-        return false;
-    // min from right child > me
-    if(root->right != NULL && minVal(root->right) <= root->val)
+    
+    if(root->val>=max || root->val<=min)
         return false;
     
-    return isValidBST(root->left) && isValidBST(root->right);
+    return valid(root->left, root->val, min) && valid(root->right, max, root->val);
+}
+
+bool isValidBST(struct TreeNode* root){
+    return valid(root, LONG_MAX, LONG_MIN);
 }
