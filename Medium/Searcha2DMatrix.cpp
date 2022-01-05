cb36ad64585bafb688864a581abc0c1fe8770b40
ienlie0513 <39272490+ienlie0513@users.noreply.github.com>
2022-01-05 17:47:18 +0800

    Update Searcha2DMatrix.cpp

--- Medium/Searcha2DMatrix.cpp
+++ Medium/Searcha2DMatrix.cpp
@@ -1,28 +1,18 @@
 class Solution {
-private:
-    bool binarySearch(vector<vector<int>>& matrix, int start, int end, int target){
-        if(start == end)
-            return false;
-        
-        int n = matrix[0].size();
-        int cur = int((start+end))/2;
-        int r = cur/n;
-        int c = cur%n;
-        
-        if(matrix[r][c] == target)
-            return true;
-        
-        if(end-start == 1)
-            return false;
-        
-        if(matrix[r][c] > target)
-            return binarySearch(matrix, start, cur, target);
-        else
-            return binarySearch(matrix, cur, end, target);
-    }
-    
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        return this->binarySearch(matrix, 0, matrix.size()*matrix[0].size(), target);
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i;
+        
+        for(i=0; i<m; i++){
+            if(matrix[i][n-1] >= target){
+                break;
+            }
+        }
+        if(i != m){
+            return binary_search(matrix[i].begin(), matrix[i].end(), target);
+        }
+        return false;
     }
 };
